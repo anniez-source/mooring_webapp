e5fe3b48a823303454d170cc8930c89ae8b0ce8a
anniez-source <znamierowski.a@northeastern.edu>
2025-10-27 19:25:03 -0400

    Add webpack watchOptions to reduce file watching

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/node_modules', '**/.next', '**/.git']
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
