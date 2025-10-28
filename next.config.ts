f0966504238f716d7dd651c685c13407b84eb6e3
anniez-source <znamierowski.a@northeastern.edu>
2025-10-27 22:20:52 -0400

    Add turbopack config to silence Next.js 16 warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {}, // Silence Next.js 16 warning
   webpack: (config) => {
     config.watchOptions = {
       ignored: ['**/node_modules', '**/.next', '**/.git']
